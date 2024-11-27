d929d2760a38a335d75ce370fe089021925b3677
Leonardo Maciel <leomaciel279@gmail.com>
2024-11-26 22:42:22 -0300

    Adicionada  mensagem de erro ao cadastrar filme

--- src/server.ts
+++ src/server.ts
@@ -20,19 +20,22 @@ app.get("/movies", async (_, res) => {
     res.json(movies);
 });
 
-app.post("/movies", async(req, res) =>{
-
+app.post("/movies", async(req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
-    await prisma.movie.create({
-        data: {
-            title,
-            genre_id,
-            language_id,
-            oscar_count,
-            release_date: new Date(release_date)
-        }
-    });
+    try{
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+    }catch(error){
+        return res.status(500).send({message: "Falha ao cadastrar um filme"});
+    }
 
     res.status(201).send();
 });
